f5a5d132972f9f3cadce4a8e1b095111251800b4
Daniel-Huang <dzhuang@caltech.edu>
2026-08-24 22:55:23 +0800

    update readme

--- scripts/darcy/multigrid_darcy_solver.py
+++ scripts/darcy/multigrid_darcy_solver.py
@@ -390,7 +390,7 @@ def cost_accuracy_traditional_solver():
             
             
             rel_error = np.linalg.norm(u_data - u_ref)/np.linalg.norm(u_ref)
-            cost[downsample, i, :] =  271*n*n + m_iteration*(88/3 * nu_iteration + 116/3), cost_cpu_time
+            cost[downsample, i, :] =  (271 + m_iteration*(88/3 * nu_iteration + 116/3))*n*n, cost_cpu_time
             accuracy[downsample, i] = rel_error
             print("relative error is : ", rel_error, " cpu_time = ", cost_cpu_time)
 
